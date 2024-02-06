8cd90a27deba94ae407845ad57670ebd162a9c93
Aisha-Moh <124839710+Aisha-Moh@users.noreply.github.com>
2024-02-06 10:16:39 +0000

    completed-articleById

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -1,20 +1,26 @@
 import { useEffect, useState } from "react";
-import { fetchArticles } from "../utils/api";
+import { fetchArticleById } from "../utils/api";
+import { useParams } from "react-router-dom";
 
 const ArticleCard = () => {
   const [articleCard, setArticleCard] = useState([]);
+  const { article_id } = useParams();
 
   useEffect(() => {
-    fetchArticles(1).then((response) => {
-      console.log(response.data.article);
+    fetchArticleById(article_id).then((response) => {
       setArticleCard(response.data.article);
     });
   }, []);
+
+  return (
+    <>
+      <h3>{articleCard.title}</h3>
+      <p>Author: {articleCard.author}</p>
+      <p>Votes: {articleCard.votes}</p>
+      <img src={articleCard.article_img_url}></img>
+      <p>{articleCard.body}</p>
+      <button>Comments</button>
+    </>
+  );
 };
-// return articleCard ?(
-//     <>
-//     <h3>{articleCard.title}</h3>
-//     <img src={articleCard.article_img_url}></img>
-//     </>
-// );
 export default ArticleCard;
