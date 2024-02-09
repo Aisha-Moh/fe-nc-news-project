ba809b89390f9ebcae639ec2ac2d4d6f33589b18
Aisha-Moh <124839710+Aisha-Moh@users.noreply.github.com>
2024-02-09 10:14:28 +0000

    delete user comment functionality

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -1,12 +1,15 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useContext } from "react";
 import { fetchComments } from "../utils/api";
 import { useParams } from "react-router-dom";
+import UserContext from "../contexts/UserContext";
+import { deleteComment } from "../utils/api";
 
 export default function Comments() {
   const [comments, setComments] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState();
   const { article_id } = useParams();
+  const { currentUser } = useContext(UserContext);
 
   useEffect(() => {
     fetchComments(article_id)
@@ -20,18 +23,32 @@ export default function Comments() {
       });
   }, []);
 
+  function handleDelete(comment_id, author) {
+    if (currentUser.username === author) {
+      console.log(comments.author, "author");
+      deleteComment(comment_id);
+      alert(
+        "Your comment has been deleted successfully! Please refresh the page."
+      );
+      //setComments();
+    } else {
+      alert("Sorry! You are not the author of this comment.");
+    }
+  }
+
   if (isLoading)
     return (
       <div className="loading-container">
         <p>{`Loading comments please wait...`}</p>
       </div>
     );
+
   if (error) return <Error error={error} />;
+
   return (
     <section>
       <div>
         <h2>Comments:</h2>
-
         <ul key="comments">
           {comments.map((comment) => {
             return (
@@ -40,6 +57,13 @@ export default function Comments() {
                   <h3>{comment.body}</h3>
                   <h4>Commented by: {comment.author}</h4>
                   <p>Votes: {comment.votes}</p>
+                  <button
+                    onClick={() =>
+                      handleDelete(comment.comment_id, comment.author)
+                    }
+                  >
+                    Delete comment
+                  </button>
                 </li>
               </>
             );
